cb10726566502f485603fe01a53c8d501b3400c0
Alex <ajbingh2@ksu.edu>
2023-08-27 18:54:34 -0500

    you can now move penguin and fish. The fish will still move off the screen though.

--- Project0/Game1.cs
+++ Project0/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Drawing.Printing;
 
 namespace Project0
 {
@@ -14,6 +16,14 @@ namespace Project0
 
         private Vector2 fishPostition;
         private Texture2D fishTexture;
+        private Vector2 fishDirection;
+
+        private SpriteFont font;
+
+        private KeyboardState currentKeyboardState;
+        private KeyboardState priorKeyboardState;
+
+        private Random rand;
 
         public Game1()
         {
@@ -26,7 +36,7 @@ namespace Project0
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            rand = new Random();
             _PenguinPosition = new Vector2(
                 GraphicsDevice.Viewport.Width / 2,
                 GraphicsDevice.Viewport.Height / 2
@@ -38,6 +48,12 @@ namespace Project0
 
             _PenguinTexture = Content.Load<Texture2D>("Penguin2");
             fishTexture = Content.Load<Texture2D>("Fish");
+            font = Content.Load<SpriteFont>("Timer");
+
+            fishDirection = new Vector2(
+                (float)rand.NextDouble(),
+                (float)rand.NextDouble()
+                );
 
             base.Initialize();
         }
@@ -45,16 +61,56 @@ namespace Project0
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            priorKeyboardState = currentKeyboardState;
+            currentKeyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            if(((int)gameTime.TotalGameTime.TotalSeconds) % 10 == 0 && (int)gameTime.TotalGameTime.TotalSeconds != 0)
+            {
+                fishDirection = new Vector2(
+                    (float)rand.NextDouble() * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds,
+                    (float)rand.NextDouble() * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds
+                    );
+            }
+
+            
+
+            fishPostition += fishDirection;
+            
+            #region Move Penguin
+            if (currentKeyboardState.IsKeyDown(Keys.Left) ||
+               currentKeyboardState.IsKeyDown(Keys.A))
+            {
+                _PenguinPosition += new Vector2(-100 * (float)gameTime.ElapsedGameTime.TotalSeconds, 0);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Right) ||
+               currentKeyboardState.IsKeyDown(Keys.D))
+            {
+                _PenguinPosition += new Vector2(100 * (float)gameTime.ElapsedGameTime.TotalSeconds, 0);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+               currentKeyboardState.IsKeyDown(Keys.W))
+            {
+                _PenguinPosition += new Vector2(0, -100 * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+               currentKeyboardState.IsKeyDown(Keys.S))
+            {
+                _PenguinPosition += new Vector2(0, 100 * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+
+            #endregion
 
             base.Update(gameTime);
         }
@@ -68,6 +124,7 @@ namespace Project0
             _spriteBatch.Begin();
             _spriteBatch.Draw(_PenguinTexture, _PenguinPosition, Color.White);
             _spriteBatch.Draw(fishTexture, fishPostition, Color.White);
+            _spriteBatch.DrawString(font, "" + gameTime.TotalGameTime.TotalSeconds.ToString(), new Vector2(700, 0), Color.Black);
             _spriteBatch.End();
 
 
