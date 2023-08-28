45e0014aebda3e234f8404a4f1c081027e969f57
Alex <ajbingh2@ksu.edu>
2023-08-28 09:58:03 -0500

    added purple fish and they won't go off the screen now

--- Project0/Game1.cs
+++ Project0/Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Drawing.Printing;
+using System.Web;
 
 namespace Project0
 {
@@ -14,16 +15,23 @@ namespace Project0
         private Vector2 _PenguinPosition;
         private Texture2D _PenguinTexture;
 
-        private Vector2 fishPostition;
-        private Texture2D fishTexture;
-        private Vector2 fishDirection;
+        private Vector2 fishPostition1;
+        private Texture2D fishTexture1;
+        private Vector2 fishDirection1;
+
+        private Vector2 fishPostition2;
+        private Texture2D fishTexture2;
+        private Vector2 fishDirection2;
 
         private SpriteFont font;
+        private String instructions = "To exit the game press the ESC key on the keyboard";
 
         private KeyboardState currentKeyboardState;
         private KeyboardState priorKeyboardState;
 
         private Random rand;
+
+        
 
         public Game1()
         {
@@ -41,19 +49,34 @@ namespace Project0
                 GraphicsDevice.Viewport.Width / 2,
                 GraphicsDevice.Viewport.Height / 2
                 );
-            fishPostition = new Vector2(
+            
+
+            _PenguinTexture = Content.Load<Texture2D>("Penguin2");
+            fishTexture1 = Content.Load<Texture2D>("Fish");
+            fishTexture2 = Content.Load<Texture2D>("FishPink20px");
+            font = Content.Load<SpriteFont>("Timer");
+
+            #region Setting up fish movement
+
+            fishPostition1 = new Vector2(
                 GraphicsDevice.Viewport.Width / 2 + 100,
                 GraphicsDevice.Viewport.Height / 2 + 50
                 );
-
-            _PenguinTexture = Content.Load<Texture2D>("Penguin2");
-            fishTexture = Content.Load<Texture2D>("Fish");
-            font = Content.Load<SpriteFont>("Timer");
-
-            fishDirection = new Vector2(
+            fishDirection1 = new Vector2(
                 (float)rand.NextDouble(),
                 (float)rand.NextDouble()
                 );
+
+            fishDirection2 = new Vector2(
+                (float)rand.NextDouble(),
+                (float)rand.NextDouble()
+                );
+            fishPostition2 = new Vector2(
+                GraphicsDevice.Viewport.Width / 2 - 100,
+                GraphicsDevice.Viewport.Height / 2 - 50
+                );
+
+            #endregion
 
             base.Initialize();
         }
@@ -75,19 +98,60 @@ namespace Project0
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            #region Move Fish Yellow
+
             // TODO: Add your update logic here
-            if(((int)gameTime.TotalGameTime.TotalSeconds) % 10 == 0 && (int)gameTime.TotalGameTime.TotalSeconds != 0)
+            if (((int)gameTime.TotalGameTime.TotalSeconds) % 10 == 0 && (int)gameTime.TotalGameTime.TotalSeconds != 0)
             {
-                fishDirection = new Vector2(
+                fishDirection1 = new Vector2(
                     (float)rand.NextDouble() * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds,
                     (float)rand.NextDouble() * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds
                     );
             }
 
-            
+            if (fishPostition1.X < GraphicsDevice.Viewport.X ||
+               fishPostition1.X > GraphicsDevice.Viewport.Width - 20) //to keep the ball totally on the scren you must account for the width of the ball
+            {
+                fishDirection1.X *= -1;
+            }
 
-            fishPostition += fishDirection;
-            
+            if (fishPostition1.Y < GraphicsDevice.Viewport.Y ||
+               fishPostition1.Y > GraphicsDevice.Viewport.Height - 20)
+            {
+                fishDirection1.Y *= -1;
+            }
+
+            fishPostition1 += fishDirection1;
+
+            #endregion
+
+            #region Move Fish Purple
+
+            // TODO: Add your update logic here
+            if (((int)gameTime.TotalGameTime.TotalSeconds) % 10 == 0 && (int)gameTime.TotalGameTime.TotalSeconds != 0)
+            {
+                fishDirection2 = new Vector2(
+                    (float)rand.NextDouble() * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds,
+                    (float)rand.NextDouble() * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds
+                    );
+            }
+
+            if (fishPostition2.X < GraphicsDevice.Viewport.X ||
+               fishPostition2.X > GraphicsDevice.Viewport.Width - 20) //to keep the ball totally on the scren you must account for the width of the ball
+            {
+                fishDirection2.X *= -1;
+            }
+
+            if (fishPostition2.Y < GraphicsDevice.Viewport.Y ||
+               fishPostition2.Y > GraphicsDevice.Viewport.Height - 20)
+            {
+                fishDirection2.Y *= -1;
+            }
+
+            fishPostition2 += fishDirection2;
+
+            #endregion
+
             #region Move Penguin
             if (currentKeyboardState.IsKeyDown(Keys.Left) ||
                currentKeyboardState.IsKeyDown(Keys.A))
@@ -123,8 +187,10 @@ namespace Project0
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(_PenguinTexture, _PenguinPosition, Color.White);
-            _spriteBatch.Draw(fishTexture, fishPostition, Color.White);
+            _spriteBatch.Draw(fishTexture1, fishPostition1, Color.White);
+            _spriteBatch.Draw(fishTexture2, fishPostition2, Color.White);
             _spriteBatch.DrawString(font, "" + gameTime.TotalGameTime.TotalSeconds.ToString(), new Vector2(700, 0), Color.Black);
+            _spriteBatch.DrawString(font, instructions, new Vector2(50, 50), Color.Black);
             _spriteBatch.End();
 
 
